dded4b506d2418f4570894bbc616264225b074cc
Adam Bader <abade011@gold.ac.uk>
2018-02-20 11:40:54 +0000

    checking first level

--- Le Vie est Belle/Assets/Script/move.cs
+++ Le Vie est Belle/Assets/Script/move.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour {
-
-	public camMove ctnv; // For accessing a variable from the cameratestnotvr script
-	public float speed = 30.0f;   // The speed the player moves at
+	
+	public float speed = 3.0f;   // The speed the player moves at
 
 	void Start() {
 
@@ -13,13 +12,7 @@ public class move : MonoBehaviour {
 
 	void Update() {
 
-		// This transform's y axis will rotate around the h variable in the cameratestnotvr script
-		this.transform.Rotate(0, ctnv.h, 0);
 
-		// The player will rotate with euler angles
-		transform.eulerAngles = new Vector3(0, ctnv.h, 0);
-
-		// WASD for movement
 		if(Input.GetKey("w")) {
 			this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
 		}
