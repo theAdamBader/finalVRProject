246341a63390a8d9a7c9b3a9a7b21522f16b70e2
Adam Bader <abade011@gold.ac.uk>
2018-02-25 15:54:31 +0000

    Audio added

---
+++ Le Vie est Belle/Assets/Script/hoverHighlights.cs
@@ -0,0 +1,38 @@
+﻿/*
+	REFERENCE:
+	Change Color on mouse over (Katus Production): https://youtu.be/VHeem-mywDk
+	Audio Source (Unity Docs): https://docs.unity3d.com/ScriptReference/AudioSource.PlayOneShot.html
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hoverHighlights : MonoBehaviour {
+
+	public AudioClip audioFile;
+	AudioSource myAudioClip;
+	public Collider box;
+
+
+	void OnMouseEnter()
+	{
+		// When moves hovers over it would turn on the light	
+		box.gameObject.GetComponent<Light> ().enabled = true;
+
+	}
+
+	void OnMouseExit()
+	{
+		// When moves hovers out it would turn off the light	
+		box.gameObject.GetComponent<Light> ().enabled = false;
+
+	}
+
+	void OnMouseDown()
+	{
+		// Mouse pressed then the audio would play
+		myAudioClip = GetComponent<AudioSource> ();
+		myAudioClip.PlayOneShot (audioFile, 0.5f);
+	}   
+}
