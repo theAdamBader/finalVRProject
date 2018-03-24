2ce519f222d27e895d7375e77f47eaf0ddea67be
Adam Bader <abade011@gold.ac.uk>
2018-03-24 14:58:01 +0000

    modified texture

--- Le Vie est Belle/Assets/Script/StartPortalTexture.cs
+++ Le Vie est Belle/Assets/Script/StartPortalTexture.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class StartPortalTexture : MonoBehaviour {
 
-	public Camera world_1Cam;
-	public Material world_1CamMat;
+	public Camera mainWorldCamReturn1;
+	public Material mainWorldCamReturn1Mat;
+
+	public Camera world1Cam;
+	public Material world1CamMat;
 
 	// Use this for initialization
 	void Start () {
-		if (world_1Cam.targetTexture != null){
 
-			world_1Cam.targetTexture.Release ();
+		if (mainWorldCamReturn1.targetTexture != null){
+
+			mainWorldCamReturn1.targetTexture.Release ();
 
 		}
-		world_1Cam.targetTexture = new RenderTexture (Screen.width, Screen.height, 32);
-		world_1CamMat.mainTexture = world_1Cam.targetTexture;
+		mainWorldCamReturn1.targetTexture = new RenderTexture (Screen.width, Screen.height, 24);
+		mainWorldCamReturn1Mat.mainTexture = mainWorldCamReturn1.targetTexture;
+
+		if (world1Cam.targetTexture != null){
+
+			world1Cam.targetTexture.Release ();
+
+		}
+		world1Cam.targetTexture = new RenderTexture (Screen.width, Screen.height, 24);
+		world1CamMat.mainTexture = world1Cam.targetTexture;
 	}
 
 }
