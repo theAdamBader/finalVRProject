1ed3709b9be69949a4d016d288824c109322e3bf
Adam Bader <abade011@gold.ac.uk>
2018-03-24 12:43:48 +0000

    added teleport

--- Le Vie est Belle/Assets/Script/playerMovement.cs
+++ Le Vie est Belle/Assets/Script/playerMovement.cs
@@ -1,20 +1,38 @@
-﻿using System.Collections;
+﻿/* 
+ REFERENCE 
+	- Movement: youtube.com/watch?v=Ov9ekwAGhMA
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+	float speed = 5.0f;
+	float grav = -9.0f;
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+	private CharacterController playerCont;
+
+	// Use this for initialization
+	void Start(){
+
+		playerCont = GetComponent<CharacterController> ();
+	}
+
+	// Update is called once per frame
+	void Update(){
+
+		float xDelta = Input.GetAxis ("Horizontal") * speed;
+		float zDelta = Input.GetAxis ("Vertical") * speed;
+		Vector3 playerMove = new Vector3 (xDelta, 0, zDelta);
+		playerMove = Vector3.ClampMagnitude (playerMove, speed);
+
+		//playerMove.y = grav;
+
+		// Ensures that the speed of the player does not change every frame
+		playerMove *= Time.deltaTime;
+		playerMove = transform.TransformDirection (playerMove);
+		playerCont.Move (playerMove);
 	}
 }
---
+++ Le Vie est Belle/Assets/Script/teleportCollider.cs
@@ -0,0 +1,57 @@
+﻿/* REFERENCE
+
+	- Teleporter: https://youtu.be/cuQao3hEKfs
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportCollider : MonoBehaviour {
+
+	// Creating a variable for a player and a receiver for when the player teleports to the other world
+
+	public Transform player;
+	public Transform reciever;
+
+	private bool playerIsOverlapping = false;
+
+	// Update is called once per frame
+	void Update () {
+		if (playerIsOverlapping)
+		{
+			Vector3 portalToPlayer = player.position - transform.position;
+			float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+			// If this is true: The player has moved across the portal
+			if (dotProduct < 0f)
+			{
+				// Teleport him!
+				float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+				rotationDiff += 180;
+				player.Rotate(Vector3.up, rotationDiff);
+
+				Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+				player.position = reciever.position + positionOffset;
+
+				playerIsOverlapping = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			playerIsOverlapping = false;
+		}
+	}
+}
