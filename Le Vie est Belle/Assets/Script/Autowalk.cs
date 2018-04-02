60303e30cc78dbb4bce47c0762558ce6ab618f8c
Adam Bader <abade011@gold.ac.uk>
2018-04-02 21:34:32 +0100

    added Gear VR auto walk function

---
+++ Le Vie est Belle/Assets/Script/Autowalk.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autowalk : MonoBehaviour {
+	public float speed = 2.0f;
+	protected bool moveForward;
+	private Transform centerCam;
+
+	private CharacterController controller;
+
+
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+
+		centerCam = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (OVRInput.Get (OVRInput.Button.PrimaryIndexTrigger) || Input.GetButtonDown ("Fire1")) {
+
+			moveForward = true;
+		} else { moveForward = false;
+		}
+
+		if (moveForward){
+
+			Vector3 forward = centerCam.TransformDirection (Vector3.forward);
+			controller.SimpleMove (forward * speed);
+
+		}
+	}
+
+
+
+}
