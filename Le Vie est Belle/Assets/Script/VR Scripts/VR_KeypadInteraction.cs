170197979bd8a8bba5b77de1fd3532033667716e
Adam Bader <abade011@gold.ac.uk>
2018-04-23 16:12:49 +0100

    add keypad and texture

---
+++ Le Vie est Belle/Assets/Script/VR Scripts/VR_KeypadInteraction.cs
@@ -0,0 +1,76 @@
+﻿/*
+	REFERENCE
+	-VR Sample (Example Interaction.cs): https://assetstore.unity.com/packages/essentials/tutorial-projects/vr-samples-51519
+*/
+
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+// This script the keyboard functions will work on VR
+public class VR_KeypadInteraction : MonoBehaviour
+{
+	// These would create a field where the developer can input private or protected variables without them being changed 
+	[SerializeField] private Material m_NormalMaterial;                
+	[SerializeField] private Material m_OverMaterial;                                         
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	[SerializeField] private Renderer m_Renderer;
+	public AudioClip audioFile;
+	[SerializeField] private AudioSource m_Audio;
+	public Collider box;
+
+	private void Awake ()
+	{
+		// Start of the game the normal material would with the dev or player changing the material directly onto it
+		m_Renderer.material = m_NormalMaterial;
+	}
+
+
+	private void OnEnable()
+	{
+		// These enable the controller or HMD to interacts with the interactable objects
+		// Which is called from the VRInput.cs
+		m_InteractiveItem.OnOver += HandleOver;
+		m_InteractiveItem.OnOut += HandleOut;
+		m_InteractiveItem.OnClick += HandleClick;
+
+	}
+
+
+	private void OnDisable()
+	{
+		// These disable the controller or HMD once it leaves the interactable objects
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+
+	//Handle the Over event
+	private void HandleOver()
+	{
+		// When over the interactable object it would change the colour of the material
+		m_Renderer.material = m_OverMaterial;
+
+	}
+
+
+	//Handle the Out event
+	private void HandleOut()
+	{
+		// When no longer interacting with the object it would default to the normal material
+		m_Renderer.material = m_NormalMaterial;
+
+	}
+
+
+	//Handle the Click event
+	private void HandleClick()
+	{
+		// When clicking on the objects, the audio would play
+		m_Audio = GetComponent<AudioSource> ();
+		m_Audio.PlayOneShot (audioFile, 0.5f);
+
+	}
+
+
+}
---
+++ Le Vie est Belle/Assets/Script/VR Scripts/VR_keypad.cs
@@ -0,0 +1,75 @@
+﻿/*
+	REFERENCE:
+	Keypad inputs (basic) by Game Design HQX: https://youtu.be/o7aC62Nsv-M
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+// This script allows the player to input a string of code
+public class VR_keypad : MonoBehaviour {
+
+	// Adding a feild to allow the VR Interact know which object to interact with
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+
+	public AudioClip audioFile;
+	AudioSource m_Audio;
+
+	// This 14 character code is what the player must put in, it is the musical note to twinkle twinkle little star
+	public static string correctInput = "0000";
+	public static string playerInput = "";
+
+	public static int totalDigits = 0;
+
+	void Start(){
+		// At the start it would get the audio source for the notes
+		m_Audio = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// If player get all the 14 notes, two of the following thing would run
+		// First, if correct then it would destroy the door at the house in the main stage
+		// Else returns back to 0 and player would rewrite the code
+		if (totalDigits == 4){
+
+			if (playerInput == correctInput)
+			{
+				Destroy(GameObject.FindWithTag("Doo"));
+			}
+
+			else {
+				playerInput = "";
+				totalDigits = 0;
+			}
+		}
+	}
+
+	private void OnEnable()
+	{
+		m_InteractiveItem.OnClick += HandleClick;
+	}
+
+
+	private void OnDisable()
+	{
+		m_InteractiveItem.OnClick -= HandleClick;
+	}
+
+
+	void HandleClick(){
+
+		// If player interacts with the game object then it would increase the total digits by one and inserts the name of the object
+		// Objects are giving name to match the string
+		playerInput += gameObject.name;
+		totalDigits ++;
+
+		// If player gets the code correct a chime would ring so that they are aware
+		if (playerInput == correctInput) {
+			m_Audio.PlayOneShot (audioFile, 0.5f);
+		}
+	}
+}
